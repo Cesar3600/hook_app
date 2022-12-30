23165e93aff1ff681b0bf03a7690e1eac0b1cf0c
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-12-30 09:16:39 -0500

    inicio de proyecto ya avanzado

---
+++ src/containers/Payment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Payment.css';
+
+const Payment = () => {
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen de pedido</h3>
+        <div className="Payment-button">Boton de pago con paypal</div>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
---
+++ src/containers/Success.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/components/Success.css';
+
+const Success = () => {
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Oscar gracias por tu compra</h2>
+        <span>Tunpedido lelgara en 3 dias a tu direccion</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
