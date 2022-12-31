be5906eaf145fa3bb38ac6fa2a78f479d47ef764
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-12-30 21:42:45 -0500

    agregago context customhook useinitialstate y uso de provider

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import InitialState from '../InitialState';
+
+const useInitialState = () => {
+  const [state, setState] = useState(InitialState);
+
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((el) => el.id !== payload.id),
+    });
+  };
+
+  return {
+    addToCart,
+    removeFromCart,
+    state,
+  };
+};
+
+export default useInitialState;
